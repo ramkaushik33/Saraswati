f9902208a8edf310bb6c30b852093c03bcf2987c
Ram Kaushik <ramkaushik44@gmail.com>
2024-09-12 19:33:31 +0530

    modifications in route

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ require("dotenv").config();
 // Initialize express app
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow only requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  credentials: true, // Allow credentials (optional, if you use cookies)
+}));
+
 // Connect to MongoDB
 connectDB();
 
@@ -14,7 +20,7 @@ connectDB();
 app.use(express.json());
 
 // Routes
-app.use("/api/auth", authRoutes);
+app.use("/", authRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
